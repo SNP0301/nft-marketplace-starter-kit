d316aa48445cfc2eaa578b6ff8fcaac2d4f10d2f
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-04-11 23:17:17 +0900

    function modifier

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -12,6 +12,17 @@ contract ERC721{
     mapping(address => uint256) private _OwnedTokensCount;
 
 
+    function balanceOf(address _owner) public view returns(uint256) {
+        require(_owner != address(0), 'owner query for non-existent ');
+        return _OwnedTokensCount[_owner];
+    }
+
+    function onwerOf(uint256 _tokenId) external view returns (address){
+        address owner = _tokenOwner[_tokenId];
+        require(owner != address(0), 'owner query for non-existent ');
+        return owner;
+    }
+
     function _exists(uint256 tokenId) internal view returns (bool){
         address owner = _tokenOwner[tokenId];
         return owner != address(0);
