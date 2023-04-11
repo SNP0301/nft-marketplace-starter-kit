aba03b9ad02c9d5eafab2d0cb2721961f93927db
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-04-11 16:06:34 +0900

    _mint() start

--- src/contracts/ERC721Metadata.sol
+++ src/contracts/ERC721Metadata.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ERC721Metadat {
+contract ERC721Metadata {
     string private _name;
     string private _symbol;
-
-    constructor(string memory named, string memory symbolified) {
+ 
+    constructor ( string memory named, string memory symbolified) {
         _name = named;
         _symbol = symbolified;
     }
