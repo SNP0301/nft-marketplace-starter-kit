15159a5a349dc50c9ecd636edfc1703adaab8bb2
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-04-12 16:21:55 +0900

    right before 150

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -10,14 +10,14 @@ contract ERC721{
 
     mapping(uint256 => address) private _tokenOwner;
     mapping(address => uint256) private _OwnedTokensCount;
-
+    mapping(uint256 => address) private _toeknApprovals;
 
     function balanceOf(address _owner) public view returns(uint256) {
         require(_owner != address(0), 'owner query for non-existent ');
         return _OwnedTokensCount[_owner];
     }
 
-    function ownerOf(uint256 _tokenId) external view returns (address){
+    function ownerOf(uint256 _tokenId) public view returns (address){
         address owner = _tokenOwner[_tokenId];
         require(owner != address(0), 'owner query for non-existent ');
         return owner;
@@ -37,5 +37,21 @@ contract ERC721{
         emit Transfer(address(0), to, tokenId);
     }
 
+    function _transferFrom(address _from, address _to, uint256 _tokenId) internal {
+        require(_to != address(0), 'Error - transfer to zero address');
+        require(ownerOf(_tokenId) == _from, 'Trying to transfer a token the address does not own');
+        
+        _OwnedTokensCount[_from] -= 1;
+        _OwnedTokensCount[_to] += 1;
+ 
+        _tokenOwner[_tokenId] = _to;
+
+        emit Transfer(_from, _to, _tokenId);
+
+    }
+
+    function transferFrom(address _from, address _to, uint256 _tokenId) public {
+        _transferFrom (_from, _to, _tokenId); 
+    }
 
 }
--- src/contracts/ERC721Enumerable.sol
+++ src/contracts/ERC721Enumerable.sol
@@ -10,9 +10,26 @@ contract ERC721Enumberable is ERC721 {
     mapping (address=>uint256[]) private _ownedTokens; 
     mapping (uint256=>uint256) private _ownedTokensIndex;
 
-    function totalSupply() external view returns (uint256){
+
+
+
+
+    function tokenOfOwnerByIndex(address owner, uint256 index) public view returns(uint256){
+        require(index <balanceOf(owner),'owner index is out of range');
+        return _ownedTokens[owner][index];
+    }
+
+    function tokenByIndex(uint256 index) public view returns(uint256) {
+        require(index < totalSupply(), 'global index is out of range');
+        return _allTokens[index];
+    }
+
+
+    function totalSupply() public view returns (uint256){
         return _allTokens.length;
     }
+
+
 
     //function tokenByIndex(uint256 _index) external view returns (uint256){}
 
@@ -20,16 +37,23 @@ contract ERC721Enumberable is ERC721 {
 
     //function tokenOfOwnerByIndex(address _owner, uint256 _index) external view returns (uint256){}
     
-
+ 
 
     function _mint(address to, uint256 tokenId) internal override(ERC721){
         super._mint(to, tokenId);
 
         _addTokensTotalSupply(tokenId);
+        _addTokensToOwnerEnumeration(to, tokenId);
     }
 
     function _addTokensTotalSupply(uint256 tokenId) private {
+        _allTokensIndex[tokenId] =_allTokens.length;
         _allTokens.push(tokenId); 
     }
  
+    function _addTokensToOwnerEnumeration(address to, uint256 tokenId) private{
+        _ownedTokensIndex[tokenId] = _ownedTokens[to].length;
+        _ownedTokens[to].push(tokenId);
+        
+    }
 } 
