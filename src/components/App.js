3edbe0b20214cb6a89999327af4a66ab5d0cf2c3
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-04-15 11:58:02 +0900

    right before design

--- src/components/App.js
+++ src/components/App.js
@@ -21,14 +21,75 @@ class App extends Component {
   }
 
   async loadBlockchainData() {
-    const accounts = await window.web3.eth.getAccounts();
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ account: accounts });
     console.log(accounts);
+
+    const networkId = await web3.eth.net.getId();
+    const networkData = KryptoBird.networks[networkId];
+    if (networkData) {
+      const abi = KryptoBird.abi;
+      const address = networkData.address;
+      const contract = new web3.eth.Contract(abi, address);
+      this.setState({ contract });
+
+      const totalSupply = await contract.methods.totalSupply().call();
+      this.setState({ totalSupply });
+
+      //let result = [];
+
+      for (let i = 1; i <= totalSupply; i++) {
+        const KryptoBird = await contract.kryptoBirdz(i - 1).call();
+
+        //result.push(kryptoBird);
+        this.setState({ kryptoBirdz: [...this.state.kryptoBirdz, KryptoBird] });
+      }
+    } else {
+      window.alert("Smart contract not deployed to detected network.");
+    }
   }
 
+  mint = (kryptoBird) => {
+    this.state.contract.methods
+      .mint(kryptoBird)
+      .send({ from: this.state.account[0] })
+      .once("recipt", (receipt) => {
+        this.setState({
+          kryptoBirdz: [...this.state.kryptoBirdz, KryptoBird],
+        });
+      });
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      accounts: "",
+      contract: null,
+      totalSupply: 0,
+      kryptoBirdz: [],
+    };
+  }
   render() {
     return (
       <div>
-        <h1>Let's Go!</h1>
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+          <div
+            className="navbar-brand col-sm-3 col-md-3 mr-0"
+            style={{ color: "white" }}
+          >
+            Krypto Birdz NFTs
+          </div>
+          <ul className="navbar-nav px-3">
+            <li
+              className="nav-item text-nowrap
+                d-none d-sm-none d-sm-block
+                "
+            >
+              <small className="text-white">{this.state.account}</small>
+            </li>
+          </ul>
+        </nav>
       </div>
     );
   }
